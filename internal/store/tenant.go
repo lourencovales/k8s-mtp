7310a30258b2cca653134d0da6d1cfb38a472a84
Lourenço Vales <133565059+lourencovales@users.noreply.github.com>
2026-04-08 16:18:01 +0200

    fix: improved sql query

--- internal/store/tenant.go
+++ internal/store/tenant.go
@@ -15,7 +15,7 @@ func (d *Database) CreateTenant(ctx context.Context, tenant *v1.Tenant) error {
 	query := `
 	INSERT INTO tenants (id, name, namespace, tier, owner_email)
 	VALUES ($1, $2, $3, $4, $5)
-	ON CONFLICT (id) DO NOTHING
+	ON CONFLICT (id, name) DO NOTHING
 	RETURNING created_at
 	`
 
